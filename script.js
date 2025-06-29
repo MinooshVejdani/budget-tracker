1da9dc4af6a27406895e0b2e4078480f32e070ad
Mehrnoush Vejdani <mi.vejdani@gmail.com>
2025-06-29 16:07:17 -0700

    Edit data handling functions and add UI handlers"

--- script.js
+++ script.js
@@ -1,60 +1,192 @@
-const amount = document.getElementById(amount);
-const date = document.getElementById(date);
-const category = document.getElementById(category);
-const id = Date.now() + Math.floor(Math.random() * 1000);
-let expense = { id: Date.now() + Math.floor(Math.random() * 1000) , amount: amount.value, 
-date: date.value,
-category: category.value
-};
+document.addEventListener("DOMContentLoaded", function () {
+  const amount = document.getElementById("amount");
+  const date = document.getElementById("date");
+  const category = document.getElementById("category");
+  const expensesContainer = document.querySelector(".expenses-container");
+  const monthSelect = document.getElementById("month-select");
 
+  const currentMonth = new Date().getMonth() + 1;
+  // monthSelect.value = currentMonth.toString();
 
-let expenses = [];
+  const formData = document.querySelector(".form-data");
+  console.log(formData);
+  formData.addEventListener("submit", (event) => {
+    event.preventDefault();
+    console.log("Form submitted!");
+    addExpense();
+    displayExpensesUI();
+    console.log(monthSelect.value); //June = '6'
+    getTotalByCategory(Number(monthSelect.value));
+    
+  });
 
+  let categories = ['Grocery', 'Clothes', 'Car Loan', 'Mortgage', 'Cosmetics', 'Travel', 'Gifts', 'Commute', 'utilities', 'mobile & data'];
 
-//Data handler functions
-const addExpense = function() {
-expenses.push(expense);
-return expenses;
-};
+  let expenses = [];
 
-const editExpense = function(id) {
-    const index = expenses.findIndex(exp => exp.id === id);
-    if(index!== -1) {
-        if(expenses[index].amount !== amount.value || expenses[index].date !== date.value || expenses[index].category !== category.value) {
-            expenses[index] = {id: id, amount: amount.value, date: date.value, category: category.value}
-        }
+  //Data handler functions
+  const addExpense = function () {
+  const expense = {
+    id: Date.now() + Math.floor(Math.random() * 1000),
+    amount: parseFloat(amount.value),
+    date: date.value,
+    category: category.value,
+  };
+
+    expenses.push(expense);
+    console.log(expenses);
+    return expenses;
+  };
+
+  const editExpense = function (id) {
+    const index = expenses.findIndex((exp) => exp.id === id);
+    if (index !== -1) {
+      if (
+        expenses[index].amount !== amount.value ||
+        expenses[index].date !== date.value ||
+        expenses[index].category !== category.value
+      ) {
+        expenses[index] = {
+          id: id,
+          amount: amount.value,
+          date: date.value,
+          category: category.value,
+        };
+      }
     }
-};
+  };
 
-const removeExpense = function(id) {
-    const index = expenses.findIndex(exp => exp.id === id);
-    if(index !== -1) {
-        expenses.splice(index, 1);
+  const removeExpense = function (id) {
+    const index = expenses.findIndex((exp) => exp.id === id);
+    if (index !== -1) {
+      expenses.splice(index, 1);
     }
-};
+  };
 
-const getTotalByCategory = function(month, category){
+  // const getTotalByCategory = function (month, category) {
+  //   let total = 0;
+  //   for (let i = 0; i < expenses.length; i++) {
+  //     const dateObj = new Date(expenses[i].date);
+  //     const expMonth = dateObj.getMonth();
+
+  //     if (expMonth === month && expenses[i].category === category) {
+  //       total += Number(expenses[i].amount);
+  //     }
+  //   }
+  //   return total;
+  // };
+
+   const getTotalByCategory = function (month) {
+    
+    let totalOfCategories = [];
+
+    categories.forEach((cat) => {
+      let total = 0;
+
+      for (let i = 0; i < expenses.length; i++) {
+      const dateObj = new Date(expenses[i].date);
+      const expMonth = dateObj.getMonth() + 1;
+
+      console.log(expMonth);
+      if (expMonth === month && expenses[i].category === cat) {
+        total += Number(expenses[i].amount);
+      }}
+      totalOfCategories.push({
+        category: cat,
+        total: total,
+      });
+    });
+  console.log(totalOfCategories);
+    return totalOfCategories;
+  };
+
+
+  const getMonthlyTotal = function (month) {
     let total = 0;
     for (let i = 0; i < expenses.length; i++) {
-        const dateObj = new Date(expenses[i].date);
-        const expMonth = dateObj.getMonth();
-        
-        if(expMonth === month && expenses[i].category === category) {
-            total += Number(expenses[i].amount);
-        }
-    }
-    return total;
-};
+      const dateObj = new Date(expenses[i].date);
+      const expMonth = dateObj.getMonth();
 
-const getMonthlyTotal = function(month) {
-let total = 0;
-    for (let i = 0; i < expenses.length; i++) {
-        const dateObj = new Date(expenses[i].date);
-        const expMonth = dateObj.getMonth();
-        
-        if(expMonth === month) {
-            total += Number(expenses[i].amount);
-        }
+      if (expMonth === month) {
+        total += Number(expenses[i].amount);
+      }
     }
     return total;
-};
+  };
+
+  const filterMonthlyExpenses = function (month) {
+    let monthExpenses = [];
+    for (let i = 0; i < expenses.length; i++) {
+      const dateObj = new Date(expenses[i].date);
+      const Month = dateObj.getMonth();
+
+      if (Month === month) {
+        monthExpenses.push(expenses[i]);
+      }
+    }
+    return monthExpenses;
+  };
+
+  //UI functions
+
+  const displayExpensesUI = function () {
+    expensesContainer.innerHTML = "";
+    expenses.forEach((exp) => {
+      const expenseDiv = document.createElement("div");
+      expenseDiv.dataset.id = exp.id;
+      expenseDiv.classList.add("expense-item");
+      expenseDiv.textContent = `Category: ${exp.category}, Amount: ${exp.amount}, Date: ${exp.date}`;
+
+      console.log(exp.category, exp.amount, exp.date);
+
+      const editButton = document.createElement("button");
+      editButton.textContent = "Edit";
+      const removeButton = document.createElement("button");
+      removeButton.textContent = "remove";
+      expenseDiv.appendChild(editButton);
+      expenseDiv.appendChild(removeButton);
+      expensesContainer.appendChild(expenseDiv);
+
+      editButton.addEventListener("click", () => {
+        editExpense(exp.id);
+      });
+
+      removeButton.addEventListener("click", () => {
+        removeExpense(exp.id);
+      });
+    });
+  };
+
+  const displayTotalByCategory = function (month) {
+
+    const totalOfCategories = getTotalByCategory(month);
+    const categoryContainer = document.createElement('div');
+    const totalByCategoryContainer = document.createElement('div');
+    categoryContainer.classList.add('category-container');
+    totalByCategoryContainer.classList.add('total-category-container');
+
+    totalOfCategories.forEach(item => {
+      const categoryDiv = document.createElement('div');
+      categoryDiv.textContent = item.category;
+      categoryContainer.appendChild(categoryDiv);
+
+      const totalDiv = document.createElement('div');
+      totalDiv.textContent = item.total;
+      totalByCategoryContainer.appendChild(totalDiv);
+
+    });
+  };
+
+  // const displayMonthlyTotal = function () {};
+
+  // const clearForm = function () {};
+
+  // const populateFormForEditing = function () {};
+
+
+  // const displayMonthlyTotal = function () {};
+
+  // const clearForm = function () {};
+
+  // const populateFormForEditing = function () {};
+});
